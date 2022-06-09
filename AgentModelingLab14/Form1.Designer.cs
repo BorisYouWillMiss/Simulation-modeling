8f6204df7221ce7fd790472999758cf1a30d76e0
astyd256 <astyd637@gmail.com>
2022-06-10 03:45:25 +0700

    Done operators simulation

--- AgentModelingLab14/Form1.Designer.cs
+++ AgentModelingLab14/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.startButton = new System.Windows.Forms.Button();
             this.labelN = new System.Windows.Forms.Label();
@@ -39,12 +39,14 @@
             this.lbAllClients = new System.Windows.Forms.Label();
             this.lbLastClientTime = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lbCurrentHour = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lbCurrentHour = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbLostClients = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.editN)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -96,7 +98,7 @@
             // lbAllClients
             // 
             this.lbAllClients.AutoSize = true;
-            this.lbAllClients.Location = new System.Drawing.Point(3, 13);
+            this.lbAllClients.Location = new System.Drawing.Point(3, 3);
             this.lbAllClients.Name = "lbAllClients";
             this.lbAllClients.Size = new System.Drawing.Size(131, 13);
             this.lbAllClients.TabIndex = 5;
@@ -105,7 +107,7 @@
             // lbLastClientTime
             // 
             this.lbLastClientTime.AutoSize = true;
-            this.lbLastClientTime.Location = new System.Drawing.Point(3, 26);
+            this.lbLastClientTime.Location = new System.Drawing.Point(3, 16);
             this.lbLastClientTime.Name = "lbLastClientTime";
             this.lbLastClientTime.Size = new System.Drawing.Size(226, 13);
             this.lbLastClientTime.TabIndex = 6;
@@ -114,13 +116,23 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lbLostClients);
             this.panel1.Controls.Add(this.lbCurrentHour);
             this.panel1.Controls.Add(this.lbAllClients);
             this.panel1.Controls.Add(this.lbLastClientTime);
-            this.panel1.Location = new System.Drawing.Point(10, 302);
+            this.panel1.Location = new System.Drawing.Point(10, 287);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(265, 67);
             this.panel1.TabIndex = 7;
+            // 
+            // lbCurrentHour
+            // 
+            this.lbCurrentHour.AutoSize = true;
+            this.lbCurrentHour.Location = new System.Drawing.Point(3, 29);
+            this.lbCurrentHour.Name = "lbCurrentHour";
+            this.lbCurrentHour.Size = new System.Drawing.Size(116, 13);
+            this.lbCurrentHour.TabIndex = 7;
+            this.lbCurrentHour.Text = "Текущее время (час):";
             // 
             // numericUpDown1
             // 
@@ -153,33 +165,33 @@
             this.dataGridView1.AllowUserToResizeColumns = false;
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.Location = new System.Drawing.Point(281, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Size = new System.Drawing.Size(307, 349);
             this.dataGridView1.TabIndex = 10;
             // 
@@ -195,20 +207,31 @@
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
             // 
-            // lbCurrentHour
+            // label2
             // 
-            this.lbCurrentHour.AutoSize = true;
-            this.lbCurrentHour.Location = new System.Drawing.Point(3, 39);
-            this.lbCurrentHour.Name = "lbCurrentHour";
-            this.lbCurrentHour.Size = new System.Drawing.Size(116, 13);
-            this.lbCurrentHour.TabIndex = 7;
-            this.lbCurrentHour.Text = "Текущее время (час):";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Current Queue:";
+            // 
+            // lbLostClients
+            // 
+            this.lbLostClients.AutoSize = true;
+            this.lbLostClients.Location = new System.Drawing.Point(3, 42);
+            this.lbLostClients.Name = "lbLostClients";
+            this.lbLostClients.Size = new System.Drawing.Size(104, 13);
+            this.lbLostClients.TabIndex = 8;
+            this.lbLostClients.Text = "Ушедшие клиенты:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
@@ -244,6 +267,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Label lbCurrentHour;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbLostClients;
     }
 }
 
--- AgentModelingLab14/Form1.cs
+++ AgentModelingLab14/Form1.cs
@@ -18,7 +18,21 @@ namespace AgentModelingLab14
         }
 
         Random rand = new Random(Guid.NewGuid().GetHashCode());
+        private int operatorNumb = 0;
+        private double[] operatorStatus;
+        private int queue = 0;
+        private int queueLim = 0;
+        private int[] freeOperatorsIndexes;
+        private int freeOperators;
+        private int lostClients = 0;
 
+
+        int[] timeOfDayForLambdas = new int[] { 6, 6, 6, 6 };
+        float[] lambdas = new float[] { 0.45f, 1.15f, 1.5f, 0.75f };
+        float currentHour = 0;
+        float currentDay = 0;
+
+        int overallClients = 0;
         // 1. Client arrival
         // 2. End of service
 
@@ -26,7 +40,30 @@ namespace AgentModelingLab14
         {
             if (startButton.Text == "Start")
             {
+                overallClients = 0;
+                currentDay = 0;
+                currentHour = 0;
+                queueLim = (int)numericUpDown1.Value;
+                queue = 0;
                 startButton.Text = "Stop";
+                operatorNumb = (int)editN.Value;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add(operatorNumb);
+                freeOperators = operatorNumb;
+                lostClients = 0;
+                for (int i = 0; i < operatorNumb; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = "Idle";
+                    dataGridView1.Rows[i].Cells[1].Value = $"Operator {i + 1}";
+                }
+                operatorStatus = new double[operatorNumb];
+                freeOperatorsIndexes = new int[operatorNumb];
+                for (int i = 0;i < operatorNumb; i++)
+                {
+                    freeOperatorsIndexes[i] = i;
+                    operatorStatus[i] = 0;
+                }
+                    
                 timer.Start();
             }
             else if (startButton.Text == "Stop")
@@ -36,19 +73,12 @@ namespace AgentModelingLab14
             }
         }
 
-        int[] timeOfDayForLambdas = new int[] { 6, 6, 6, 6};
-        float[] lambdas = new float[] { 0.45f, 1.15f, 1.5f,  0.75f};
-        float currentTimeOfDay = 0;
-        float currentDay = 0;
-
-        int overallClients = 0;
-
         public bool SimulateClientArrivalPPP()
         {
             float curLambda = 0f;
             //Finding lambda for time of day
             int k = 0;
-            float currentTimeDay = currentTimeOfDay;
+            float currentTimeDay = currentHour;
             while(currentTimeDay >= 0)
             {
                 currentTimeDay -= timeOfDayForLambdas[k];
@@ -57,12 +87,12 @@ namespace AgentModelingLab14
             }
             curLambda = lambdas[k];
 
-            currentTimeDay = currentTimeOfDay;
+            currentTimeDay = currentHour;
 
             while (true)
             {
                 //New possible time for arrival (newTimeOfDayArrival can be > 24 (for day counting purposes))
-                float newTimeOfDayArrival = currentTimeOfDay + (-(float)Math.Log(rand.NextDouble()) / curLambda);
+                float newTimeOfDayArrival = currentHour + (-(float)Math.Log(rand.NextDouble()) / curLambda);
                 float hourOfArrival = newTimeOfDayArrival;
                 if(hourOfArrival > 24) hourOfArrival -= 24;
                     
@@ -86,14 +116,14 @@ namespace AgentModelingLab14
                 if (newLambda >= u)
                 {
                     //Lets gooooo
-                    float timeHoursDif = Math.Abs(hourOfArrival - currentTimeOfDay); //0.5hour - 23.5hour (should be 1 hour dif) (reality = 23hours)
-                    if (hourOfArrival < currentTimeOfDay)
+                    float timeHoursDif = Math.Abs(hourOfArrival - currentHour); //0.5hour - 23.5hour (should be 1 hour dif) (reality = 23hours)
+                    if (hourOfArrival < currentHour)
                     {
-                        float first = Math.Abs(24 - currentTimeOfDay);
+                        float first = Math.Abs(24 - currentHour);
                         timeHoursDif = Math.Abs(hourOfArrival + first);
                     }
 
-                    currentTimeOfDay = hourOfArrival;
+                    currentHour = hourOfArrival;
 
                     currentDay += timeHoursDif / 24f;
                     overallClients++;
@@ -107,7 +137,41 @@ namespace AgentModelingLab14
             SimulateClientArrivalPPP();
             lbAllClients.Text = "Всего клиентов пришло: " + overallClients.ToString();
             lbLastClientTime.Text = "Время прихода последнего клиента (день): " + String.Format("{0:N3}", currentDay);
-            lbCurrentHour.Text = "Текущее время (час): " + String.Format("{0:N3}", currentTimeOfDay);
+            lbCurrentHour.Text = "Текущее время (час): " + String.Format("{0:N3}", currentHour);
+
+            // Counting all work done
+            for (int i = 0; i < operatorNumb; i++)
+            {
+                if (currentDay * 100 > operatorStatus[i] && operatorStatus[i] != 0)
+                {
+                    operatorStatus[i] = 0;
+                    freeOperators++;
+                    freeOperatorsIndexes[freeOperators - 1] = i;
+                    dataGridView1.Rows[freeOperatorsIndexes[freeOperators - 1]].Cells[0].Value = "Idle";
+                }
+            }
+
+            // Another dude came
+            queue++;
+            double a = 0;
+            for (; freeOperators > 0 & queue > 0; queue--) // Putting all people into free places
+            {
+                a = -Math.Log(rand.NextDouble()) / lambdas[(int)(currentHour / 6)];  // part of formula where (-ln(a)/lambda)
+                operatorStatus[freeOperatorsIndexes[freeOperators - 1]] = (int)((currentDay + a / 24) * 100 + a % 24);
+                dataGridView1.Rows[freeOperatorsIndexes[freeOperators - 1]].Cells[0].Value = "Working";
+                freeOperatorsIndexes[freeOperators - 1] = -1;
+                freeOperators--;
+            }
+
+            if (queue > queueLim)
+            {
+                lostClients += queue - queueLim;
+                queue = queueLim;
+            }
+            label2.Text = $"Current Queue {queue}";
+            lbLostClients.Text = $"Ушедшие клиенты: {lostClients}";
+            
+
         }
     }
 }

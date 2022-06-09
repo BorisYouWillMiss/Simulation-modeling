634f68b88b349d841162985511af826c5434985c
Nixion <den-kosarev@mail.ru>
2022-06-09 22:35:01 +0700

    Added client labels

--- AgentModelingLab14/Form1.Designer.cs
+++ AgentModelingLab14/Form1.Designer.cs
@@ -33,19 +33,25 @@
             this.startButton = new System.Windows.Forms.Button();
             this.labelN = new System.Windows.Forms.Label();
             this.editN = new System.Windows.Forms.NumericUpDown();
+            this.lbAllClients = new System.Windows.Forms.Label();
+            this.lbLastClientTime = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.editN)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer
             // 
             this.timer.Interval = 200;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // startButton
             // 
             this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.startButton.Location = new System.Drawing.Point(17, 105);
+            this.startButton.Location = new System.Drawing.Point(13, 85);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(126, 48);
+            this.startButton.Size = new System.Drawing.Size(94, 39);
             this.startButton.TabIndex = 0;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
@@ -55,31 +61,65 @@
             // 
             this.labelN.AutoSize = true;
             this.labelN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelN.Location = new System.Drawing.Point(12, 9);
+            this.labelN.Location = new System.Drawing.Point(9, 7);
+            this.labelN.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelN.Name = "labelN";
-            this.labelN.Size = new System.Drawing.Size(26, 25);
+            this.labelN.Size = new System.Drawing.Size(20, 20);
             this.labelN.TabIndex = 3;
             this.labelN.Text = "N";
             // 
             // editN
             // 
             this.editN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.editN.Location = new System.Drawing.Point(44, 7);
+            this.editN.Location = new System.Drawing.Point(33, 6);
+            this.editN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.editN.Name = "editN";
-            this.editN.Size = new System.Drawing.Size(101, 30);
+            this.editN.Size = new System.Drawing.Size(76, 26);
             this.editN.TabIndex = 4;
+            // 
+            // lbAllClients
+            // 
+            this.lbAllClients.AutoSize = true;
+            this.lbAllClients.Location = new System.Drawing.Point(3, 13);
+            this.lbAllClients.Name = "lbAllClients";
+            this.lbAllClients.Size = new System.Drawing.Size(131, 13);
+            this.lbAllClients.TabIndex = 5;
+            this.lbAllClients.Text = "Всего клиентов пришло:";
+            // 
+            // lbLastClientTime
+            // 
+            this.lbLastClientTime.AutoSize = true;
+            this.lbLastClientTime.Location = new System.Drawing.Point(3, 26);
+            this.lbLastClientTime.Name = "lbLastClientTime";
+            this.lbLastClientTime.Size = new System.Drawing.Size(226, 13);
+            this.lbLastClientTime.TabIndex = 6;
+            this.lbLastClientTime.Text = "Время прихода последнего клиента (день):";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lbAllClients);
+            this.panel1.Controls.Add(this.lbLastClientTime);
+            this.panel1.Location = new System.Drawing.Point(12, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 52);
+            this.panel1.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.editN);
             this.Controls.Add(this.labelN);
             this.Controls.Add(this.startButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.editN)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +131,9 @@
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Label labelN;
         private System.Windows.Forms.NumericUpDown editN;
+        private System.Windows.Forms.Label lbAllClients;
+        private System.Windows.Forms.Label lbLastClientTime;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- AgentModelingLab14/Form1.cs
+++ AgentModelingLab14/Form1.cs
@@ -17,6 +17,8 @@ namespace AgentModelingLab14
             InitializeComponent();
         }
 
+        Random rand = new Random(Guid.NewGuid().GetHashCode());
+
         // 1. Client arrival
         // 2. End of service
 
@@ -32,6 +34,39 @@ namespace AgentModelingLab14
                 startButton.Text = "Start";
                 timer.Stop();
             }
+        }
+
+        int[] timeOfDayForLabdas = new int[] { 6, 12, 18, 24};
+        float[] lambdas = new float[] { 0f, 1f, 1.5f,  0.35f};
+        float currentTimeOfDay = 0;
+        float currentDay = 0;
+
+        public bool SimulateClientArrivalPPP()
+        {
+            float curLambda = 0f;
+            //Finding lambda for time of day
+            int k = 0;
+            float currentTime = currentTimeOfDay;
+            while(currentTime >=0)
+            {
+                currentTime -= timeOfDayForLabdas[k];
+                if (currentTime < 0) break;
+                else k++;
+            }
+            curLambda = lambdas[k];
+
+            currentTime = currentTimeOfDay;
+
+            //New possible time for arrival
+            float newTimeofDayArrival = currentTimeOfDay + (-(float)Math.Log(rand.NextDouble()) / curLambda);
+            //newTimeofDayArrival = newTimeofDayArrival 
+
+            return true;///
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            SimulateClientArrivalPPP();
         }
     }
 }
